77aee97ff92c7d96ce4f1f2d2caf6f254d47b1fc
MaI1ce <vladyslavshapoval21@gmail.com>
2022-01-16 15:34:16 +0100

    Add minor changes and comments

--- _ifconfig.c
+++ _ifconfig.c
@@ -1,14 +1,22 @@
+/*
+    Program to study basics of ioctl interface
+    last change: 16.01.2022
+    autor      : Vladyslav Shapoval
+*/
+
 #include <sys/ioctl.h>//input-output control
+#include <sys/socket.h>//struct sockaddr
 #include <net/if.h> 
 #include <unistd.h>
-#include <netinet/in.h>
-#include <string.h>
+#include <netinet/in.h>//struct sockaddr_in 
+#include <string.h>//strcpy
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <linux/if_arp.h>
-#include <arpa/inet.h>
+#include <linux/if_arp.h>//ioctl's for ARP
+#include <arpa/inet.h> //char *inet_ntoa(struct in_addr in);
 
+/*struct to save info from driver*/
 struct interface {
     char   name[256];
     int    index;
@@ -18,6 +26,88 @@ struct interface {
     short  mac_family;
     char   mac[8];
 } inf;
+
+void show_info (struct interface * info);
+
+int main (void) 
+{
+
+    char buf[1024];
+
+    struct ifconf ifc;
+    ifc.ifc_len = sizeof(buf);
+    ifc.ifc_buf = buf;
+    
+    int temp_errno = 0;
+    
+    /*open socket*/
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(sock == -1){ /*handle error*/
+        temp_errno = errno;
+        printf("socket error: %s \n",strerror(temp_errno));
+        close(sock);
+        return -1;
+    }
+
+    /* get list of interfaces*/
+    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { 
+        /* handle error */ 
+        temp_errno = errno;
+        printf("ioctl error: %s \n",strerror(temp_errno));
+        close(sock);
+        return -1;
+    }
+
+    /*find end of the list*/
+    struct ifreq* it = ifc.ifc_req;
+    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+
+    /*list of requests to driver*/
+    unsigned long int  request[5] = {SIOCGIFINDEX, SIOCGIFFLAGS, SIOCGIFADDR,
+                                     SIOCGIFNETMASK, SIOCGIFHWADDR};
+
+
+    struct sockaddr_in* src = NULL;
+    struct interface req;
+    int i = 0;
+    /*for each interface in the list*/
+    for (; it != end; ++it) {
+        strcpy(req.name, it->ifr_name);
+        for(i = 0; i < 5; ++i){
+            /*request to driver*/
+            if (ioctl(sock, request[i], it )== -1) {
+                temp_errno = errno;
+                printf("ioctl error: %s \n",strerror(temp_errno));
+                close(sock);
+                return -1;
+            }
+            switch(i){
+                case 0:
+                    req.index = it->ifr_ifindex;
+                    break;
+                case 1:
+                    req.flags = it->ifr_flags;
+                    break;
+                case 2:
+                    src = (struct sockaddr_in*)&(it->ifr_addr);
+                    req.addr.s_addr = src->sin_addr.s_addr;
+                    break;
+                case 3:
+                    src = (struct sockaddr_in*)&(it->ifr_netmask);
+                    req.mask.s_addr = src->sin_addr.s_addr;
+                    break;
+                case 4:
+                    req.mac_family = it->ifr_hwaddr.sa_family;
+                    memcpy(req.mac, it->ifr_hwaddr.sa_data, 6);
+            }
+        }
+        show_info(&req);
+    }
+
+    close(sock);
+    return 0;
+}
+
 
 void show_info (struct interface * info)
 {
@@ -42,82 +132,25 @@ void show_info (struct interface * info)
         printf("\tnot an Ethernet interface\n");
     }
 }
+/*
+#include <netinet/in.h>
 
-int main (void) 
-{
+struct sockaddr_in {
+    short            sin_family;   // e.g. AF_INET
+    unsigned short   sin_port;     // e.g. htons(3490)
+    struct in_addr   sin_addr;     // see struct in_addr, below
+    char             sin_zero[8];  // zero this if you want to
+};
 
-    char buf[1024];
+struct in_addr {
+    unsigned long s_addr;  // load with inet_aton()
+};
 
-    struct ifreq ifr;
-    struct ifconf ifc;
-    ifc.ifc_len = sizeof(buf);
-    ifc.ifc_buf = buf;
-    
-    int temp_errno = 0;
-    
-    /*open socket*/
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(sock == -1){ /*handle error*/
-        temp_errno = errno;
-        printf("socket error: %s \n",strerror(temp_errno));
-        close(sock);
-        return -1;
-    }
-    /* get list of interfaces*/
-    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { 
-        /* handle error */ 
-        temp_errno = errno;
-        printf("ioctl error: %s \n",strerror(temp_errno));
-        close(sock);
-        return -1;
-    }
 
-    /*find end of the list*/
-    struct ifreq* it = ifc.ifc_req;
-    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+#include <sys/socket>
 
-    unsigned long int  request[5] = {SIOCGIFINDEX, SIOCGIFFLAGS, SIOCGIFADDR,
-                                     SIOCGIFNETMASK, SIOCGIFHWADDR};
-
-    struct sockaddr_in* src = NULL;
-    struct sockaddr_in* dst = NULL;
-    struct ifreq temp_req;
-    struct interface req;
-    int i = 0;
-    for (; it != end; ++it) {
-        strcpy(temp_req.ifr_name, it->ifr_name);
-        strcpy(req.name, it->ifr_name);
-        for(i = 0; i < 5; ++i){
-            if (ioctl(sock, request[i], &temp_req )== -1) {
-                temp_errno = errno;
-                printf("ioctl error: %s \n",strerror(temp_errno));
-                close(sock);
-                return -1;
-            }
-            switch(i){
-                case 0:
-                    req.index = temp_req.ifr_ifindex;
-                    break;
-                case 1:
-                    req.flags = temp_req.ifr_flags;
-                    break;
-                case 2:
-                    src = (struct sockaddr_in*)&(temp_req.ifr_addr);
-                    req.addr.s_addr = src->sin_addr.s_addr;
-                    break;
-                case 3:
-                    src = (struct sockaddr_in*)&(temp_req.ifr_netmask);
-                    req.mask.s_addr = src->sin_addr.s_addr;
-                    break;
-                case 4:
-                    req.mac_family = temp_req.ifr_hwaddr.sa_family;
-                    memcpy(req.mac, temp_req.ifr_hwaddr.sa_data, 6);
-                    break;
-            }
-        }
-        show_info(&req);
-    }
-
-    close(sock);
-    return 0;
-}
+struct sockaddr{
+    sa_family_t   sa_family       address family
+    char          sa_data[]       socket address (variable-length data)
+};
+*/
